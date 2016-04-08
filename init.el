86ace03c54f526aa81797c38a632836e5479270f
cedille <cedille@localhost.localdomain>
2016-04-08 00:49:03 -0400

    Update info

--- init.el
+++ init.el
@@ -4,15 +4,28 @@
 (when (< emacs-major-version 24)
   ;; For important compatibility libraries like cl-lib
   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+;; (append package-load-list 'zenburn)
+(package-initialize)
+
+;;change theme
+;; (load-theme 'zenburn t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("f3d6a49e3f4491373028eda655231ec371d79d6d2a628f08d5aa38739340540b" default)))
+ '(electric-pair-mode t)
+ '(package-load-list (quote (all zenburn))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+
+(package-refresh-contents)
 (package-initialize) ;; You might already have this line
-
-
-;; (global-set-key (kbd "M-p") 'ace-window)
-
-
-(require 'windmove)
-(global-set-key (kbd "C-c <left>")  'windmove-left)
-(global-set-key (kbd "C-c <right>") 'windmove-right)
-(global-set-key (kbd "C-c <up>")    'windmove-up)
-(global-set-key (kbd "C-c <down>")  'windmove-down)
-
