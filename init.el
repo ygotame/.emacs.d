0482d0cedd53312fb601dcc6bd0b084a21ff185c
cedille <cedille@localhost.localdomain>
2016-04-07 23:35:07 -0400

    Initialisation du repos

---
+++ init.el
@@ -0,0 +1,18 @@
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/"))
+(when (< emacs-major-version 24)
+  ;; For important compatibility libraries like cl-lib
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+(package-initialize) ;; You might already have this line
+
+
+;; (global-set-key (kbd "M-p") 'ace-window)
+
+
+(require 'windmove)
+(global-set-key (kbd "C-c <left>")  'windmove-left)
+(global-set-key (kbd "C-c <right>") 'windmove-right)
+(global-set-key (kbd "C-c <up>")    'windmove-up)
+(global-set-key (kbd "C-c <down>")  'windmove-down)
+
